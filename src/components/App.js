0cce90ef1c1de09243743dd97ddbe5d5ed7f1e37
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-20 10:30:37 +0530

    normally working

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,55 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+    // write your code here
+    var timer;
+    const [inputTime, setInputTime] = useState(0);
+    const countDownTimer = (e) => {
+        if (e.which === 13) {
+            clearInterval(timer);
+            setInputTime(0);
+            let input = Math.floor(document.getElementById("timeCount").value);
+            if (input === "0" || input === "") return;
+            input = Number(input);
+            // console.log("insude function");
+            setInputTime(input);
+            timer = setInterval(() => {
+                console.log("timeron");
+                setInputTime((prev) => prev - 1);
+            }, 1000);
+        }
+    };
+    useEffect(() => {
+        if (inputTime <= 0) {
+            clearInterval(timer);
+            setInputTime(0);
+        }
+    }, [inputTime]);
 
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
-      </div>
-      <div id="current-time">{/* remaining time */}</div>
-    </div>
-  )
-}
+    useEffect(() => {
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
 
+    return (
+        <div className="wrapper">
+            <div id="whole-center">
+                <h1>
+                    Reverse countdown for
+                    <input
+                        id="timeCount"
+                        onKeyDown={(e) => {
+                            countDownTimer(e);
+                        }}
+                    />{" "}
+                    sec.
+                </h1>
+            </div>
+            <div id="current-time">{/* remaining time */ inputTime}</div>
+        </div>
+    );
+};
 
 export default App;
